bb3164987d5c6dbbb45987c9ad850b7fa031da1d
Tsvetkov Maxim <pushiniiist@gmail.com>
2023-10-26 17:54:38 +0200

    lab6 done

--- lab6.py
+++ lab6.py
@@ -1,5 +1,16 @@
 def selection_sort(arr):
     for i in range(len(arr)):
-        min_index = i
+        min_element = arr[i]
         for j in range(i + 1, len(arr)):
-            if
+            if arr[j] < min_element:
+                min_element = arr[j]
+                min_element_index = arr.index(arr[j])
+        if arr[i] != min_element:
+            arr[i], arr[min_element_index] = arr[min_element_index], arr[i]
+    return arr
+
+
+arr = [64, 25, 12, 22, 11]
+print(selection_sort(arr))
+
+

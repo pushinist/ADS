9bb0e4ed5527c3ddc207a5ecb32da735ce4a861d
Tsvetkov Maxim <pushiniiist@gmail.com>
2023-10-28 22:11:11 +0200

    lab 9 done

---
+++ lab9.py
@@ -0,0 +1,40 @@
+def siftDown(lst, i, upper):
+    while True:
+        l, r = i * 2 + 1, i * 2 + 2
+        if max(l, r) < upper:
+            if lst[i] >= max(lst[l], lst[r]):
+                break
+            elif lst[l] > lst[r]:
+                lst[i], lst[l] = lst[l], lst[i]
+                i = l
+            else:
+                lst[i], lst[r] = lst[r], lst[i]
+                i = r
+        elif l < upper:
+            if lst[l] > lst[i]:
+                lst[l], lst[i] = lst[i], lst[l]
+                i = l
+            else:
+                break
+        elif r < upper:
+            if lst[r] > lst[i]:
+                lst[r], lst[i] = lst[i], lst[r]
+                i = r
+            else:
+                break
+        else:
+            break
+
+
+def heapsort(lst):
+    for j in range((len(lst) - 2) // 2, -1, -1):
+        siftDown(lst, j, len(lst))
+
+    for end in range(len(lst) - 1, 0, -1):
+        lst[0], lst[end] = lst[end], lst[0]
+        siftDown(lst, 0, end)
+
+
+lst = list(map(int, input("Введите последовательность чисел, разделяя их пробелом: ").split()))
+heapsort(lst)
+print(f"Отсортированный список: {lst}, результат сортировки - {lst == sorted(lst)}")
